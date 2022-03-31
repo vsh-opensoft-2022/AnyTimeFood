b48dfc52b56caf18c970d876b8856de6522598e6
YogeshGaddam <yogesh.gaddam@gmail.com>
2022-03-31 16:31:19 +0530

    updated apis

--- controllers/userinfo.ts
+++ controllers/userinfo.ts
@@ -6,6 +6,7 @@ exports.getUserinfo = async (req: Request, res: Response) => {
     const uid = Number(req.params.uid);
     dbConn.query(`select * from users where ID = ${uid}`, (err: any, result: any) => {
         if (err) console.log('error while fetching data');
+        console.log("fetched user data.");
         res.status(200).send(result);
     });
 }
@@ -13,6 +14,7 @@ exports.getUserinfo = async (req: Request, res: Response) => {
 exports.getUserAddresses = async (req: Request, res: Response) => {
     dbConn.query(`select * from addresses where userID = ${req.params.uid}`, (err: any, result: any) => {
         if (err) console.log('error while fetching data');
+        console.log("fetched user addresses.");
         res.status(200).send(result);
     });
 }
@@ -22,6 +24,7 @@ exports.addAddress = async (req: Request, res: Response) => {
     dbConn.query(
         `insert into addresses (address,street,city,state,pincode) values ('${newitem.name}','${newitem.category}',${newitem.nonveg},'${newitem.price}')`, (err: any) => {
             if (err) console.log(err);
+            console.log("new address added.");
             res.status(201).send("Item added successfully!");
         }
     );
@@ -30,6 +33,7 @@ exports.addAddress = async (req: Request, res: Response) => {
 exports.deleteAddressById = async (req: Request, res: Response) => {
     dbConn.query(`delete from addresses where id='${req.params.id}'`, (err: any) => {
         if (err) console.log(err);
+        console.log("address deleted successfully.");
         res.status(202).send("Item deleted successfully");
     });
 }
